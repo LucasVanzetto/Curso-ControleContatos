421675561d7763c9f19f6b2ddbbe9f0ba28d0d7f
Lucas Vanzetto <lucas.c.vanzetto@gmail.com>
2023-10-06 18:24:57 -0300

    Aula 7

--- ControleContatos/Controllers/UsuarioController.cs
+++ ControleContatos/Controllers/UsuarioController.cs
@@ -23,6 +23,42 @@ namespace ControleContatos.Controllers
       return View();
     }
 
+    public IActionResult ApagarConfirmacao(int id)
+    {
+      UsuarioModel usuario = _usuarioRepositorio.ListarPorId(id);
+      return View(usuario);
+    }
+
+    public IActionResult Apagar(int id)
+    {
+      try
+      {
+        bool apagado = _usuarioRepositorio.Apagar(id);
+
+        if (apagado)
+        {
+          TempData["MensagemSucesso"] = "Usuário excluído com sucesso";
+        }
+        else
+        {
+          TempData["MensagemErro"] = $"Erro ao excluir, tente novamente";
+        }
+        return RedirectToAction("Index");
+      }
+      catch (Exception erro)
+      {
+        TempData["MensagemErro"] = $"Erro ao excluir, tente novamente - {erro.Message}";
+        return RedirectToAction("Index");
+      }
+    }
+
+    public IActionResult Editar(int id)
+    {
+      UsuarioModel usuario = _usuarioRepositorio.ListarPorId(id);
+      return View(usuario);
+    }
+
+
     [HttpPost]
     public IActionResult Criar(UsuarioModel usuario)
     {
@@ -40,6 +76,38 @@ namespace ControleContatos.Controllers
       catch (Exception erro)
       {
         TempData["MensagemErro"] = $"Erro ao cadastrar, tente novamente - {erro.Message}";
+        return RedirectToAction("Index");
+      }
+    }
+
+    [HttpPost]
+    public IActionResult Editar(UsuarioSemSenhaModel usuarioSemSenha)
+    {
+      try
+      {
+        UsuarioModel usuario = null;
+
+        if (ModelState.IsValid)
+        {
+          usuario = new UsuarioModel()
+          {
+            Id = usuarioSemSenha.Id,
+            Nome = usuarioSemSenha.Nome,
+            Login = usuarioSemSenha.Login,
+            Email = usuarioSemSenha.Email,
+            Perfil = usuarioSemSenha.Perfil
+          };
+
+          usuario = _usuarioRepositorio.Editar(usuario);
+          TempData["MensagemSucesso"] = "Usuário alterado com sucesso";
+          return RedirectToAction("Index");
+        }
+
+        return View(usuario);
+      }
+      catch (Exception erro)
+      {
+        TempData["MensagemErro"] = $"Erro ao editar, tente novamente - {erro.Message}";
         return RedirectToAction("Index");
       }
     }
--- ControleContatos/Models/UsuarioSemSenhaModel.cs
+++ ControleContatos/Models/UsuarioSemSenhaModel.cs
@@ -3,26 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ControleContatos.Models
 {
-  public class UsuarioModel
+  public class UsuarioSemSenhaModel
   {
     public int Id { get; set; }
 
-    [Required(ErrorMessage = "Digite o nome de usuário")]
+    [Required(ErrorMessage = "Digite o nome do usuário")]
     public string Nome { get; set; }
 
-    [Required(ErrorMessage = "Digite o nome de login")]
+    [Required(ErrorMessage = "Digite o login do usuário")]
     public string Login { get; set; }
 
-    [Required(ErrorMessage = "Digite o nome de e-mail")]
+    [Required(ErrorMessage = "Digite o email do usuário")]
     [EmailAddress(ErrorMessage = "E-mail inválido!")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "Digite o nome de senha")]
-    public string Senha { get; set; }
-
     [Required(ErrorMessage = "Selecione o perfil do usuário")]
     public PerfilEnum? Perfil { get; set; }
-    public DateTime DataCadastro { get; set; }
-    public DateTime? DataAtualizacao { get; set; }
   }
 }
